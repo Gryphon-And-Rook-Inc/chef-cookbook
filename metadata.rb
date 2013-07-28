6f605b368f245f38f04e40480e1339207e19733b
David Higgins <david@adamo.ca>
2013-07-28 08:10:21 -0600

    Initial empty cookbook

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'creoadamo'
+maintainer       'David Higgins'
+maintainer_email 'creo@adamo.ca'
+license          'All rights reserved'
+description      'Installs/Configures creoadamo recipes'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
