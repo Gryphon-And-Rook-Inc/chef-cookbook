6311d1d98b5af81b3b5e6e4342423a47d9bfdf95
David Higgins <david@gryphonandrook.com>
2016-04-28 12:33:32 -0600

    Rename to Gryphon & Rook

--- metadata.rb
+++ metadata.rb
@@ -1,19 +1,19 @@
-name             'creoadamo'
+name             'gnr_cookbook'
 maintainer       'David Higgins'
-maintainer_email 'creo@adamo.ca'
+maintainer_email 'david@gryphonandrook.com'
 license          'All rights reserved'
-description      'Installs/Configures creoadamo recipes'
+description      'Installs/Configures Gryphon & Rook recipes'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
-recipe           "creoadamo::apt_update",       "Update apt-get"
-recipe           "creoadamo::fish_shell",       "Compile, Install, and link Fish shell"
-recipe           "creoadamo::groups",           "Create and manage groups"
-recipe           "creoadamo::nodejs",           "Install Nodejs and Package Manager"
-recipe           "creoadamo::ruby-1.9.3-p362",  "Install ruby_build, rbenv, and ruby 1.9.3-p362"
-recipe           "creoadamo::ruby-2.0.0-p195",  "Install ruby_build, rbenv, and ruby 2.0.0-p195"
-recipe           "creoadamo::ruby-2.0.0-p247",  "Install ruby_build, rbenv, and ruby 2.0.0-p247"
-recipe           "creoadamo::ruby-2.1.1",       "Install ruby_build, rbenv, and ruby 2.1.1"
-recipe           "creoadamo::ruby-2.1.1-p76",   "Install ruby_build, rbenv, and ruby 2.1.1-p76"
+recipe           'gryphonandrook::apt_update',       'Update apt-get'
+recipe           'gryphonandrook::fish_shell',       'Compile, Install, and link Fish shell'
+recipe           'gryphonandrook::groups',           'Create and manage groups'
+recipe           'gryphonandrook::nodejs',           'Install Nodejs and Package Manager'
+recipe           'gryphonandrook::ruby-1.9.3-p362',  'Install ruby_build, rbenv, and ruby 1.9.3-p362'
+recipe           'gryphonandrook::ruby-2.0.0-p195',  'Install ruby_build, rbenv, and ruby 2.0.0-p195'
+recipe           'gryphonandrook::ruby-2.0.0-p247',  'Install ruby_build, rbenv, and ruby 2.0.0-p247'
+recipe           'gryphonandrook::ruby-2.1.1',       'Install ruby_build, rbenv, and ruby 2.1.1'
+recipe           'gryphonandrook::ruby-2.1.1-p76',   'Install ruby_build, rbenv, and ruby 2.1.1-p76'
 
 %w{ rbenv ruby_build users chef-solo-search apt build-essential git }.each do |cb|
   depends cb
