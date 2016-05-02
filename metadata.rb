27326338853fa61a651e523ff339f776608d138f
David Higgins <david@gryphonandrook.com>
2016-05-02 11:37:08 -0600

    Update metadata

--- metadata.rb
+++ metadata.rb
@@ -1,14 +1,7 @@
-name             'gnr_cookbook'
-maintainer       'David Higgins'
-maintainer_email 'david@gryphonandrook.com'
-license          'All rights reserved'
-description      'Installs/Configures Gryphon & Rook recipes'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
-recipe           'opsworks-cookbook-custom-env::custom_env',  'Install Custom Env for OpsWorks'
-recipe           'opsworks_custom_env::configure',   'Configure Custom Env for OpsWorks'
-recipe           'whenever::default',                'Configure Whenever OpsWorks'
-
-%w{ rbenv ruby_build users chef-solo-search apt build-essential git }.each do |cb|
-  depends cb
-end
+name              'gnr_cookbook'
+maintainer        'David Higgins'
+maintainer_email  'david@gryphonandrook.com'
+license           'All rights reserved'
+description       'Installs/Configures Gryphon & Rook recipes'
+long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version           '0.2.0'
