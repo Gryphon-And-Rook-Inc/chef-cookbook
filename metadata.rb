09a9aa7b1338d26cf7417c3c5c2a66efcec81bb4
gryphonandrook <38541934+gryphonandrook@users.noreply.github.com>
2021-02-25 19:29:17 -0700

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -4,4 +4,5 @@ maintainer_email  'david@gryphonandrook.com'
 license           'All rights reserved'
 description       'Installs/Configures Gryphon & Rook recipes'
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+source_url 'https://github.com/Gryphon-And-Rook-Inc/chef-cookbook'
 version           '0.2.1'
